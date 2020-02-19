c73f5f525f03d9921dac1178a1231ebc454ea6a2
Tommy Connor <tconnor98@aol.com>
2020-02-19 20:34:11 +0000

    Automatically backed up by Learn

---
+++ lib/doctor.rb
@@ -0,0 +1,30 @@
+class Doctor
+  attr_accessor :name 
+  
+  @@all = []
+  
+  def initialize(name)
+    @name = name
+    save
+  end
+  
+  def save
+    @@all << self
+  end
+  
+  def new_appointment(date, patient, doctor = self)
+    Appointment.new(date, patient, doctor)
+  end
+  
+  def appointments
+    Appointment.all.select {|date| date.doctor == self}
+  end
+  
+  def patients
+    self.appointments.select {|date| date.patient}
+  end
+  
+  def self.all
+    @@all
+  end
+end
